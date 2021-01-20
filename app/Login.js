51cc95ee2c9e0e29faf6159bb5dd83486cad71c2
lhabib lachil <habib.lachil@gmail.com>
2021-01-20 17:29:37 +0100

    Login2

--- app/Login.js
+++ app/Login.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
-import {Text, StyleSheet,Image} from 'react-native';
+import {Text, StyleSheet,Image,View,TextInput,TouchableOpacity} from 'react-native';
 import {Card, CardItem, Button, Input, Spinner} from './common';
 import {connect} from 'react-redux';
 import {onLogin} from './redux';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+
 
 
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
   errorStyle: {
     fontSize: 17,
     alignSelf: 'center',
@@ -23,6 +30,31 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: 'hidden',
+    backgroundColor: 'white',
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingLeft: 16,
+  },
+  SLink: {
+    color: '#788eec',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  fText: {
+    fontSize: 16,
+    color: '#2e2e2d',
+  },
+  fView: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 20,
   },
 });
 
@@ -66,42 +98,55 @@ class Login  extends Component {
 
     render(){
         return (
-        <Card title= "Anmeldungsform">
-          <CardItem> 
-          <Image
+          <View style={styles.container}>
+             <KeyboardAwareScrollView
+        style={{ flex: 1, width: '100%' }}
+        keyboardShouldPersistTaps='always'
+      >
+      <Image
           style={styles.logo}
           source={require('./assets/icon.png')}
         />
-        </CardItem>
-            <CardItem> 
-            <Input label='Benutzername' placeholder='Benutzername eingeben!' secureTextEntry={false} 
-              onChangeText={(username) => this.setState({ username  }) }
-            />
-            </CardItem>
-            <CardItem>
-            <Input label='Kennwort' placeholder='Kennwort eingeben!' secureTextEntry={true}
-            onChangeText={(password) => this.setState({ password }) } />
-            </CardItem>
+        <TextInput
+          style={styles.input}
+          placeholder='Benutzername eingeben!'
+          placeholderTextColor='#aaaaaa'
+          secureTextEntry={false} 
+          onChangeText={(username) => this.setState({ username  }) }
+          underlineColorAndroid='transparent'
+          autoCapitalize='none'
+        />
+        <TextInput
+       style={styles.input}
+       placeholderTextColor='#aaaaaa'
+       secureTextEntry
+       placeholder='Kennwort eingeben!'secureTextEntry={true}
+       onChangeText={(password) => this.setState({ password }) }
+       underlineColorAndroid='transparent'
+       autoCapitalize='none'
+     />
+     <TouchableOpacity style={styles.button} onPress={() => onLoginPress()}>
+          <Button style={styles.buttonTitle}>Log in</Button>
+        </TouchableOpacity>
+        <View style={styles.fView}>
+          <Text style={styles.fText}>
+            Don&apos;t have an account?{' '}
+            <Text  style={styles.SLink} onPress = {this.home.bind(this)} >
+              Sign up
+            </Text>
+          </Text>
+        </View>
+      </KeyboardAwareScrollView>
+       </View>);}}
+       
 
-            <CardItem>{ this._renderButton() }
-            </CardItem>
-            <Text style={styles.errorStyle}>{this.props.error}</Text>
-            
-            <CardItem>
-              <Button onPress = {this.home.bind(this)}><Text>Neu Hier? Regestieren!</Text></Button>
-            </CardItem>
-        
-        </Card>
-        );
-    }
-};
 
-const mapStateToProps = state => {
-    return {
-      error: state.auth.error,
-      loading: state.auth.loading,
-      user: state.auth.user
-    }
-  }
+      function mapStateToProps(state) {
+        return {
+          error: state.auth.error,
+          loading: state.auth.loading,
+          user: state.auth.user
+        };
+      }
   
   export default connect(mapStateToProps, { onLogin })(Login);
